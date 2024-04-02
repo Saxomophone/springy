1178b1a021a83e8745837e78dadbf39abeb86941
Danny Edwards <danny_edwards09@icloud.com>
2024-04-03 01:25:26 +0800

    fixed incorrect graph: was initially parsing in x and y as strings so matplotlib assumed both were categorical. Now it plots correctly and as expected oscillates

--- src/plot.py
+++ src/plot.py
@@ -4,20 +4,21 @@ import csv
 with open ('generated/spring.csv', 'r') as file:
   reader = csv.reader(file, delimiter=',')
 
-  velocity = [];
-  time = [];
+  velocity = []
+  length = []
+  time = []
 
   for row in reader:
     if row[0] == 'time':
       continue
-    time.append(row[0])
-    velocity.append(row[1])
+    time.append(int(row[0]))
+    velocity.append(round(float(row[1]), 3))
+    length.append(round(float(row[2]), 3))
 
-plt.figure(figsize=(8,6))
-plt.plot(time, velocity)
-plt.xlabel('Time')
-plt.ylabel('Velocity')
-plt.title('Spring Velocity')
 
+plt.plot(time, velocity, '-b', label='Velocity')
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m/s)')
+plt.title('Velocity of a spring over time')
 plt.grid(True)
 plt.savefig('generated/spring_velocity.png')
